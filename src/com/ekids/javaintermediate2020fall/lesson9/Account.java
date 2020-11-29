71ac6667838819901849e3dcd6c535ba31bddc18
a_yurkevich <melchior.by@gmail.com>
2020-11-29 16:58:39 +0300

    add lesson 9 - Generics

---
+++ src/com/ekids/javaintermediate2020fall/lesson9/Account.java
@@ -0,0 +1,24 @@
+package com.ekids.javaintermediate2020fall.lesson9;
+
+class Account<T> {
+
+    private T id;
+    private int sum;
+
+    Account(T id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
